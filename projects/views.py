00ad77824523f096c09a0965a94fc041ee648730
NitishOritro <nitish.csedu51@gmail.com>
2023-05-29 14:18:35 +0600

    Template: URL & Views

--- projects/views.py
+++ projects/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-from django.http import HttpResponse
+
+def projectsUrlFunc(request):
+    return render(request, 'projects.html')
 
 
+
+#Passing Dynamic Data
+
+def singleProjectPage(request, id):
+    #return HttpResponse("Here are our Single project products url page: Id is: "+str(id))
+    return render(request, 'single-project.html')
+
+#URL Based views
+
+
+'''
 def projectsUrlFunc(request):
     return HttpResponse("Here are our products url page")
 
 
-'''
-Passing Dynamic Data
-'''
+
+#Passing Dynamic Data
 
 def singleProjectPage(request, id):
     return HttpResponse("Here are our Single project products url page: Id is: "+str(id))
+
+
+'''
